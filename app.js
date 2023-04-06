bdf225e62126223e0db3c83e3befcb9d4eb7e1a1
Ersin Sevinç <ersinsevnc@gmail.com>
2023-04-06 12:21:09 +0300

    nodemon command added, express setup

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//App entrypoint
+const express = require('express');
+
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.send('app started'));
+
+app.listen(PORT, () => console.log(`app running on port: ${PORT}`));
